72373f3b5fb2bcc28f920432d03debacb94d9111
Alex Batrak <ab@pufetto.com>
2019-03-25 16:48:43 +0200

    Works, but no variables from login.php

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
     session_start();
-    if (isset($session_login) && $session_login !=""){echo "Welcome, ".$session_login;} else {?>
-    <HTML>
+?>
+    
+<HTML>
     <HEAD>
         <TITLE>First PHP Script</TITLE>
-        </HEAD>
+    </HEAD>
     <BODY>
 
-    <div style="border: 0px solid blue; position:relative; top:100px; left:400px; height:200px; width:300px;">
+    	<div style="border: 0px solid blue; position:relative; top:100px; left:400px; height:200px; width:300px;">
 	
-     <form action="/login.php" method="post">
-        <label>Login:</label><br/>
-        <input name="login" type="text" size="15" maxlength="15"><br/>
-        <label>Password:</label><br/>
-        <input name="password" type="password" size="15" maxlength="15"><br/><br/>
-        <input type="submit" value="Submit"><br/><br/>
-    </form>}?>
-    
-	</div>
-	
+		<?php 
+		echo $session_login;
+	   if (isset($session_login) && $session_login !="") {echo "Welcome, ".$session_login;} 
+	   else {?>
+			<form action="/login.php" method="post">
+   			<label>Login:</label><br/>
+			<input name="login" type="text" size="15" maxlength="15"><br/>
+			<label>Password:</label><br/>
+			<input name="password" type="password" size="15" maxlength="15"><br/><br/>
+			<input type="submit" value="Submit"><br/><br/>
+    		</form><?php }?>    
+		</div>
     </BODY>
-
 </HTML>
